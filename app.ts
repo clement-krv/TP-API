7350effa52983616c922da6790741e8411f0f86c
Clément Kerviche <kerviche.clement@gmail.com>
2023-11-17 15:27:11 +0100

    protection des donné lors de add-user

--- app.ts
+++ app.ts
@@ -1,13 +1,15 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import fs from 'fs';
 import basicAuth from 'express-basic-auth';
 import low, { LowdbSync } from 'lowdb';
 import FileSync from 'lowdb/adapters/FileSync';
+import * as yup from 'yup';
 
 interface User {
+  id?: number;
   email: string;
   password: string;
+  role: 'STUDENT' | 'ADMIN';
 }
 
 interface DBSchema {
@@ -33,23 +35,41 @@ app.use(basicAuth({
   unauthorizedResponse: 'Nom d\'utilisateur ou mot de passe incorrect',
 }));
 
+const userSchema = yup.object().shape({
+  email: yup.string().email().required().defined(),
+  password: yup.string().required().defined(),
+  role: yup.string().oneOf(['STUDENT', 'ADMIN']).required().defined(),
+}).noUnknown().strict().required().defined();
+
 app.get('/', (req, res) => {
   res.status(200).send({ message: 'Vous êtes connecté!' });
 });
 
 app.post('/add-users', async (req, res) => {
-  const newUser: User = req.body;
+  let newUser: User;
 
-  // Vérifiez que le nouvel utilisateur a un email et un mot de passe
-  if (!newUser.email || !newUser.password) {
-    return res.status(400).send({ message: 'Le nom d\'utilisateur et le mot de passe sont requis' });
+  try {
+    newUser = userSchema.validateSync(req.body);
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      return res.status(400).send({ message: 'Les données fournies sont invalides : ' + error.errors.join(', ') });
+    }
+    // handle other types of errors
+    return res.status(500).send({ message: 'Une erreur inattendue s\'est produite' });
   }
+
+  // Supprimez les champs inconnus
+  newUser = userSchema.cast(newUser);
 
   // Vérifiez que l'utilisateur n'existe pas déjà
   const existingUser = db.get('users').find({ email: newUser.email }).value();
   if (existingUser) {
-    return res.status(409).send({ message: 'Un utilisateur avec ce nom d\'utilisateur existe déjà' });
+    return res.status(409).send({ message: 'Un utilisateur avec cet email existe déjà' });
   }
+
+  // Générez un nouvel ID pour le nouvel utilisateur
+  const newId = db.get('users').size().value() + 1;
+  newUser.id = newId;
 
   // Ajoutez le nouvel utilisateur à la base de données
   db.get('users').push(newUser).write();
