a9f08602feb8339cd9c73952502326c2a1c037c0
Clément Kerviche <kerviche.clement@gmail.com>
2023-11-17 16:35:04 +0100

    Ajout de la table students_courses

--- app.ts
+++ app.ts
@@ -19,13 +19,22 @@ interface Course {
   heure: string;
 }
 
+interface StudentCourse {
+  id?: number;
+  studentId: number;
+  courseId: number;
+  registeredAt: string | null;
+  signedAt: string | null; 
+}
+
 interface DBSchema {
   users: User[];
   courses: Course[];
+  studentCourses: StudentCourse[];
 }
 
 const adapter = new FileSync<DBSchema>('db.json');
-const db: LowdbSync<DBSchema> = low(adapter);
+const db = low(adapter);
 
 const app = express();
 app.use(bodyParser.json());
@@ -60,6 +69,13 @@ const courseSchema = yup.object().shape({
   title: yup.string().required().defined(),
   date: yup.string().matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d$/, 'La date doit être au format jj/mm/AAAA').required().defined(),
   heure: yup.string().matches(/^([01]\d|2[0-3]):([0-5]\d)$/, 'L\'heure doit être au format HH:MM en 24 heures').required().defined(),
+}).noUnknown().strict().required().defined();
+
+const studentCourseSchema = yup.object().shape({
+  studentId: yup.number().required().defined(),
+  courseId: yup.number().required().defined(),
+  registeredAt: yup.string().matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d (0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, 'La date et l\'heure doivent être au format jj/mm/AAAA HH:MM').defined(),
+  signedAt: yup.string().matches(/^(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d (0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/, 'La date et l\'heure doivent être au format jj/mm/AAAA HH:MM').nullable().default(null),
 }).noUnknown().strict().required().defined();
 
 function checkRole(role: string) {
@@ -138,6 +154,54 @@ app.post('/add-courses', checkRole('ADMIN'), (req, res) => {
 
   // Renvoyez une réponse de succès
   res.status(201).send({ message: 'Cours ajouté avec succès' });
+});
+
+app.post('/add-studentcourse', checkRole('ADMIN'), (req, res) => {
+  let newStudentCourse: StudentCourse = {
+    ...req.body,
+    registeredAt: new Date().toLocaleString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit' }),
+  };
+
+  try {
+    newStudentCourse = studentCourseSchema.validateSync(newStudentCourse);
+  } catch (error) {
+    if (error instanceof yup.ValidationError) {
+      return res.status(400).send({ message: 'Les données fournies sont invalides : ' + error.errors.join(', ') });
+    }
+    // handle other types of errors
+    return res.status(500).send({ message: 'Une erreur inattendue s\'est produite' });
+  }
+
+  // Supprimez les champs inconnus
+  newStudentCourse = studentCourseSchema.cast(newStudentCourse);
+
+  // Vérifiez que le courseId existe
+  const existingCourse = db.get('courses').find({ id: newStudentCourse.courseId }).value();
+  if (!existingCourse) {
+    return res.status(400).send({ message: 'Le cours avec cet ID n\'existe pas' });
+  }
+
+  // Vérifiez que le studentId existe
+  const existingUser = db.get('users').find({ id: newStudentCourse.studentId }).value();
+  if (!existingUser) {
+    return res.status(400).send({ message: 'L\'utilisateur avec cet ID n\'existe pas' });
+  }
+
+  // Générez un nouvel ID pour le nouveau StudentCourse
+  const newId = db.get('studentCourses').value().length + 1;
+  newStudentCourse.id = newId;
+
+  // Définissez registeredAt à la date et l'heure actuelles
+  const now = new Date();
+  const date = now.toLocaleDateString('fr-FR', { day: '2-digit', month: '2-digit', year: 'numeric' });
+  const time = now.toLocaleTimeString('fr-FR', { hour: '2-digit', minute: '2-digit' });
+  newStudentCourse.registeredAt = `${date} ${time}`;
+
+  // Ajoutez le nouveau StudentCourse à la base de données
+  db.get('studentCourses').push(newStudentCourse).write();
+
+  // Renvoyez une réponse de succès
+  res.status(201).send({ message: 'Inscription de l\'étudiant au cours ajoutée avec succès' });
 });
 
 app.listen(3000, () => {
