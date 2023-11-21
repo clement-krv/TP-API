e2040a8f6fcca17f34b7bd61cceff0c872f8f401
Clément Kerviche <kerviche.clement@gmail.com>
2023-11-21 01:31:20 +0100

    Ajout final de tailwind + style de la page index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./public/**/*.html,js"],
+  content: ["./public/**/*.html", "./src/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require("@tailwindcss/forms"),
+  ],
 }
 
